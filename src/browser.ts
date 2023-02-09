e8778f12278e3d6594da12d6d3ae511c524483f5
Piyush Raj <piyush.189303055@muj.manipal.edu>
2023-02-10 01:16:09 +0530

    minor: fail fast on environment mismatch

--- src/browser.ts
+++ src/browser.ts
@@ -1,3 +1,10 @@
 import { createDefaultBFF } from './presets/default'
 
+if (typeof window === 'undefined') {
+  throw new Error(
+    '[BFF] Browser build loaded in a non-browser environment. ' +
+    'Use the ESM/CJS build instead.'
+  )
+}
+
 ;(window as any).BFF = createDefaultBFF()
