c58dcd7f17a46efd4743e86f978afebe36576b26
Piyush Raj <piyush.189303055@muj.manipal.edu>
2023-02-26 19:53:50 +0530

    feat: scaffold `anyHardBlockRule` policy
    An opinionated policy rule which triggers a block action
    when signals with the block flag meet a confidence threshold.

---
+++ src/policy/rules/hardBlock.ts
@@ -0,0 +1,17 @@
+import { PolicyRule } from '@bff/policy/rules'
+
+export const anyHardBlockRule = (
+  minConfidence = 0.7
+): PolicyRule => result => {
+  const hasHardBlock = result.signals.some(
+    s => s.block && s.confidence >= minConfidence
+  )
+
+  return hasHardBlock
+    ? {
+        action: 'block',
+        reason: 'Hard blocking signal present',
+        confidence: result.confidence
+      }
+    : null
+}
