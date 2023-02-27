2f09dbe3509f2e9a9375764b664a3aa3aa8db6a7
Piyush Raj <piyush.189303055@muj.manipal.edu>
2023-02-27 22:42:16 +0530

    minor: use `anyHardBlockRule` in aggressive preset

--- src/presets/policy/aggressive.ts
+++ src/presets/policy/aggressive.ts
@@ -1,7 +1,9 @@
 import { PolicyRule } from '@bff/policy/types'
 import { confidenceRule } from '@bff/policy/ruleHelpers'
+import { anyHardBlockRule } from '@bff/policy/rules'
 
 export const aggressivePolicy: PolicyRule[] = [
+  anyHardBlockRule(0.6),
   confidenceRule(0.7, 'block', 'High confidence'),
   confidenceRule(0.4, 'challenge', 'Suspicious behavior')
 ]
